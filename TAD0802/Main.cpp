30accc5b0a1061b2f91638f8e98f0e996e533410
Milly <millydack@gmail.com>
2017-03-19 15:07:06 +0100

    Add files via upload

---
+++ TAD0802/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include "Queue.h"
+
+using namespace std;
+
+int main() {
+	int numCasos;
+	int n;
+	cin >> numCasos;
+	for (int i = 0; i < numCasos; i++) {
+		Queue<int>* q = new Queue<int>();
+		cin >> n;
+		while (n != -1) {
+			q->push_back(n);
+			cin >> n;
+		}
+		cin >> n;
+		cout << *q;
+		q->reverseFirstK(n);
+		cout << *q;
+		delete q;
+	}
+}
