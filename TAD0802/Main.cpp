5f087264094b0656ff95f53d06851dab4a098f0b
Milly <millydack@gmail.com>
2017-03-19 15:08:59 +0100

    Update Main.cpp

--- TAD0802/Main.cpp
+++ TAD0802/Main.cpp
@@ -3,6 +3,10 @@
 #include "Queue.h"
 
 using namespace std;
+/*
+* No encuentro el fallo en este ejercicio, funcionaba con todas las pruebas realizadas así que si sirve de inspiración para alguien
+* estaría genial
+*/
 
 int main() {
 	int numCasos;
